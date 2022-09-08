e588ede867c81d4f8241a9ba075272601737f009
legendosky <bintinlayemuhammed@gmail.com>
2022-09-08 14:30:17 -0700

    Hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return:always 0
+ * Return: always 0
  */
 int main(void){
-	
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
