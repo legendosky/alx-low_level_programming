2ccfbf411cdc1c96480ecec23a3ee35083cf8229
legendosky <bintinlayemuhammed@gmail.com>
2022-09-08 15:30:21 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /**
  * main - Entry point
  *
- * Return always 0
+ * Description: using the main function
+ *
+ * Return: always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf ( "with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 
 }
