e9c6117cdbccaa97600a7181dce753a184f5a64c
legendosky <bintinlayemuhammed@gmail.com>
2022-09-08 16:21:12 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long 1i;
+	long long 11i;
+	float f;
+
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("size of an int: %d byte(s)\n", sizeof(i));
+printf("size of a long int: %1d byte(s)\n", sizeof(li));
+printf("size of a long long int: %1d byte(s)\n", sizeof(11i));
+printf("size of a float: %1d byte(s)\n", sizeof(f));
+return(0);
+}
