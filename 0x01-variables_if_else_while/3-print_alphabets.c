128b818aa1edd9004d7b0aec4f57ec48b66f90b6
legendosky <bintinlayemuhammed@gmail.com>
2022-09-09 15:23:47 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	char j;
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
