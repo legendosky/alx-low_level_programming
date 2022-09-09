166899fcb88dd001e07fc7ac924e884d5828a530
legendosky <bintinlayemuhammed@gmail.com>
2022-09-09 01:28:13 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,14 @@
 #include <unistd.h>
 
 /**
- *
  * main - Entry point
  *
+ * Description: using the main function
  * Return: Always 1 (Success)
- *
  */
 int main(void)
 {
 write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
