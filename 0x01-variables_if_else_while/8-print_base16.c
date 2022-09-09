eb27ea052410be8acf6a424cfaa4d09762f725e0
legendosky <bintinlayemuhammed@gmail.com>
2022-09-09 16:38:23 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 48; i < 58; i++)
+	{
+		
+		putchar(i);
+	}
+	for (j = 'a'; j <= 'f' j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
