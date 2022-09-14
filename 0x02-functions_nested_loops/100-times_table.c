423900c52139191f3256ae400d47863f227cbb5c
legendosky <bintinlayemuhammed@gmail.com>
2022-09-14 01:54:12 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,47 +3,39 @@
  * print_timees_table - prints times table, starting with 0
  * @n: number of the times table to use
  */
-
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
-
-	if (n < 0 || n > 15)
-		return;
-	
-	while (a <= n)
-	{
-		for (b = 0; b <= n; b++)
-		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-		}
-		_putchar('\n');
-		a++;
-	}
+int i, j, k;
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = j * i;
+if (j == )
+{
+_putchar(k + '0');
+}
+else if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10 && k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
 }
