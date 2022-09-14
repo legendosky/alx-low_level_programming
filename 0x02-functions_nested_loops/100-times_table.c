25cd84eefc27eb3a693824ec42530de0639db678
legendosky <bintinlayemuhammed@gmail.com>
2022-09-14 04:58:24 -0700

    100_times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * print_times_table - Prints times table of the input,
  *                      starting with 0.
  * @n: The value of the times table to be printed.
- */
+ *
 void print_times_table(int n)
 {
 	int i, j, times;
@@ -31,6 +31,7 @@ void print_times_table(int n)
 					_putchar((times % 10) + 48);
 				} else if (times > 99 && times < 1000)
 				{
+
 					_putchar((times / 100) + 48);
 					_putchar(((times / 10) % 10) + 48);
 					_putchar((times % 10) + 48);
