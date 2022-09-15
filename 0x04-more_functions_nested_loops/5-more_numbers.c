5e48d38b3ab2d7a26b96c0327ecad98b73792d71
legendosky <bintinlayemuhammed@gmail.com>
2022-09-15 08:39:46 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
+ * more_numbers - prints more numbers
  */
+
 void more_numbers(void)
 {
 	int i, j;
@@ -20,7 +20,9 @@ void more_numbers(void)
 			}
 			_putchar((j % 10) + 48);
 		}
+
 		_putchar('\n');
+
 		i++;
 	}
 }
